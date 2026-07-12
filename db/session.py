e9d580ca20e04d7b8925a440be5e5b860d9140a4
Narasimha Reddy Koduri <157327654+knreddy03@users.noreply.github.com>
2026-07-11 23:50:39 -0500

    feat: configure PostgreSQL and SQLAlchemy database layer

---
+++ db/session.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+from core.config import settings
+
+
+engine = create_engine(
+    settings.database_url, 
+    pool_pre_ping=True
+    )
+
+SessionLocal = sessionmaker(
+    autocommit=False, 
+    autoflush=False, 
+    bind=engine
+    )
